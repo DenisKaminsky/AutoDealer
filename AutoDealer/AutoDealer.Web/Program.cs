1f08d5c5f4cf72309f76858bb62418cfb2d054f1
DenisKaminsky <36419821+DenisKaminsky@users.noreply.github.com>
2020-04-28 01:20:55 +0300

    Modified gitingnore

--- AutoDealer/AutoDealer.Web/Program.cs
+++ AutoDealer/AutoDealer.Web/Program.cs
@@ -1,5 +1,7 @@
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace AutoDealer.Web
 {
@@ -7,14 +9,19 @@ namespace AutoDealer.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(config)
+                .UseStartup<Startup>();
+        }
+
     }
 }
